1d07c828ba450765abcb829cfaaddc845c6d5492
Sayna <Sayna@git.com>
2020-05-13 15:57:35 +0200

    first try kmp

--- src/knuth_morris_pratt.py
+++ src/knuth_morris_pratt.py
@@ -19,19 +19,67 @@ def naive(text, pattern):
     return matchList
 
 
+def createNextTable(pattern):
+
+    prea = [0] * len(pattern)
+    j, t = 1, 0
+
+    while j < len(pattern)-1:
+
+        while t > 0 and pattern[j] != pattern[t]:
+
+            t = prea[t]
+
+        j = j+1
+        t = t + 1
+        if pattern[j] == pattern[t]:
+            prea[j] = prea[t]
+        else:
+            prea[j] = t
+
+        #print("j: ", j, prea[j])
+
+    return prea
+
+# only finds first match
+
+
 def knuthMorrisPratt(text, pattern):
-    pass
+
+    textLen, patLen = len(text), len(pattern)
+    text = " " + text
+    pattern = " " + pattern
+
+    # next table
+    prea = createNextTable(pattern)
+    print(prea)
+
+    patPos = textPos = 1
+    while patPos <= patLen and textPos <= textLen:
+
+        while patPos > 0 and text[textPos] != pattern[patPos]:
+
+            patPos = prea[patPos]
+
+        textPos = textPos+1
+        patPos = patPos+1
+
+        if patPos > patLen:
+            # because of leading " " -1
+            print(textPos-patLen-1)
 
 
-# simple test cases
-if __name__ == "__main__":
-    text = "This is a example text with two is."
-    pattern = "is"
-    atext = "aaaaaaaaab"
-    apattern = "aaaab"
+def testBoth(text, pattern):
 
     print(f"Testing normal text:\np:{pattern}\nt:{text}")
     print(naive(text, pattern))
 
-    print(f"Testing a text:\np:{apattern}\nt:{atext}")
-    print(naive(atext, apattern))
+    print(f"\nWith KMP:")
+    knuthMorrisPratt(text, pattern)
+
+
+# simple test cases
+if __name__ == "__main__":
+
+    testBoth("This is a example text with two is.", "is")
+    testBoth("babcbabcabcaabcabcabcacabc", "abcabcacab")
