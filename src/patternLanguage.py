87556b3afe337b1e676e501db5058bd4ff8badce
Sayna <Sayna@git.com>
2021-01-20 14:54:27 +0100

    begining of desPat

--- src/patternLanguage.py
+++ src/patternLanguage.py
@@ -1,12 +1,12 @@
-from knuth_morris_pratt import naive
+from knuth_morris_pratt import naive, knuthMorrisPratt
 
-# single word -> string
+# single terminal word -> string
 w1 = "abbaabaa"
 w2 = "baabbabaabba"
 w3 = "abaaaba"
 
 
-# pattern -> string, wo großbuchstaben variablen sind
+# pattern -> string, where capital letters are variables, lower case are terminal symbols
 alpha = "aAbaBa"
 
 # set of variable assignments -> list of strings for each variabel, the first one fills the first variabel and so on
@@ -24,8 +24,21 @@ def fillPattern(pattern, variables):
         else:
             word += c
 
-    print(word)
     return word
+
+
+def canonicalForm(pattern):
+    # transforms the pattern into canonical form, for now each variable is unique
+
+    pattern = list(pattern)
+
+    firstVariable = "A"
+    for i, c in enumerate(pattern):
+        if c.isupper():
+            pattern[i] = firstVariable
+            firstVariable = chr(ord(firstVariable) + 1)
+
+    return "".join(pattern)
 
 
 def findAllNonVariables(pattern):
@@ -48,39 +61,66 @@ def findAllNonVariables(pattern):
     return words
 
 
-# matching problem for regular pattern, m is number of variables
-
-
 def matchingRegular(pattern, word):
-    """
-    # prefix check
-    count = 0
-    c = pattern[count]
-    while c.islower():
-        count += 1
-        c = pattern[count]
-    if pattern[:count] != word[:count]:
-        return False
-    """
+    # matching problem for regular pattern
+
+    # check prefix
+    j = 0
+    while pattern[j].islower():
+        if pattern[j] != word[j]:
+            return False
+        j += 1
 
     # main check
     w_i = findAllNonVariables(pattern)
-
-    print(w_i)
-
-    j = 0
+    suffix = w_i.pop()
 
     for s in w_i:
-        print(j, word[j:], s)
-        find = naive(word[j:], s)
+
+        find = knuthMorrisPratt(word[j:], s)
         if find == []:
             return False
         else:
             j += find[0] + len(s)
 
+    # check suffix
+    if suffix != word[-len(suffix) :]:
+        return False
+
     return True
 
 
-beta = "aAbaBc"
-wort = "abbaabaac"
-print(matchingRegular(beta, wort))
+def descPat(sample, word):
+
+    m = len(word)
+
+    alpha, var = [], "A"
+    for _ in range(m):
+        alpha.append(var)
+        var = chr(ord(var) + 1)
+
+    for i in range(m):
+        q, j = True, 1
+        print(alpha)
+        # if canonicalForm(fillPattern())
+
+    return alpha
+
+
+"""
+problem: membership problem with variables
+fillPattern for single variable
+"""
+
+if __name__ == "__main__":
+
+    # test membership
+    beta = "aAbaBc"
+    word = "abbAabaac"
+    # print(matchingRegular(beta, word))
+
+    # print(canonicalForm("AbsdfdsfsdfDefgegegC"))
+
+    sample = ["abc", "abbc", "abbbc"]
+    word = "abc"
+    print(descPat(sample, word))
